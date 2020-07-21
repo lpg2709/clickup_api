1718f2586286127ed324d63c7fde4046bc4e3a04
lpg2709 <leonardo_guarezi@hotmail.com>
2020-07-20 21:25:30 -0300

    Add Teams

---
+++ src/components/Teams.js
@@ -0,0 +1,20 @@
+const Team = require("../models/Teams")
+
+var Teams = function (token) {
+	this.token = token;
+}
+
+Teams.prototype.get_teams = function () {
+	token = this.token;
+	return new Promise(async function (resolve, reject) {
+		try {
+			var res = await Team.get_teams();
+			resolve(res);
+		} catch (err) {
+			reject(err);
+		}
+	});
+}
+
+
+module.exports = Teams;
--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ const Task = require('./components/Tasks');
 const Tags = require("./components/Tags");
 const Spaces = require("./components/Spaces");
 const Lists = require("./components/Lists");
+const Teams = require("./components/Teams");
 
 var Clickup = function(token){
 	this.token = token;
@@ -10,6 +11,7 @@ var Clickup = function(token){
 	this.Tags = new Tags(this.token);
 	this.Spaces = new Spaces(this.token);
 	this.Lists = new Lists(this.token);
+	this.Teams = new Teams(this.token);
 }
 
 module.exports = Clickup;
