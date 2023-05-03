d61b499cedbeeb4db09e22c056a52ca24618a2be
Leonardo Padilha Guarezi <leonardo_guarezi@hotmail.com>
2023-05-03 11:08:03 -0300

    ADD new member TaskRelationships

---
+++ exemples/TaskRelationships/delete_task_link.js
@@ -0,0 +1,17 @@
+const clickup_api = require("../../src/index");
+
+(async function main() {
+	const Clickup = new clickup_api("access_token");
+
+	var params = {
+		custom_task_ids: true,
+		team_id: "team_id" // Only used when the custom_task_ids parameter is set to true.
+	}
+
+	try {
+		var info = await Clickup.TaskRelationships.delete_task_link("task_id", "links_to", params);
+		console.log(info);
+	} catch (err) {
+		console.log(err)
+	}
+})()
